107dc9132fe1f62d8eeaa36a7db5d2c9281b39f0
Pol Martín Garcia <pol147258369@gmail.com>
2021-12-07 18:56:30 +0100

    Update resume

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:C:\Users\SirKoto\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5@v5.0.0-20211025220623-c43d078def74\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\SirKoto\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5@v5.0.0-20211122231030-d26f01d5d6c3\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
